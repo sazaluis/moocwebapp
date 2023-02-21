6dfa0e8c77198f66d5fed02a09408be2e953016c
sazaluis <sazaluis@gmail.com>
2023-02-22 00:17:58 +0100

    part 5.3  Validación de formularios

--- mooc/U6/app/server-final-u4.py
+++ mooc/U6/app/server-final-u4.py
@@ -265,3 +265,4 @@ if __name__ == '__main__':
         app.run(debug=True, port=8080)
     else:
         app.run(debug=True, port=80)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
